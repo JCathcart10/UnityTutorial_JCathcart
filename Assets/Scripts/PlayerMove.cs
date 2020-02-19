b63094d7b6c822cd1638f7e5b6edaa6fbc31c98b
jacob <jaccathc@uat.edu>
2020-02-18 18:03:18 -0700

    Finished Tutorial

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    public float speedMod = 1;
+
+    private int count;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate()
+    { 
+        float mSpeedHori = Input.GetAxis("Horizontal");
+        float mSpeedVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(mSpeedHori * speedMod, 0, mSpeedVert * speedMod);
+
+        rb.AddForce(movement);
+
+        if (Input.GetButtonDown("Cancel"))// detects when the player presses esc
+        {
+            Application.Quit();// quits game
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
+    
 }
